81d43f0976dfddd80ca3ca6e652adda396ec7373
dodoan <dodoan.it@gmail.com>
2018-12-24 22:51:38 +0700

    - background prefab + adding following camera.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    private float distanceToTarget;
+
+    private void Start()
+    {
+        distanceToTarget = transform.position.x - target.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x + distanceToTarget, transform.position.y, transform.position.z);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jetpackForce = 75.0f;
+    public float forwardMovementSpeed = 3.0f;
     private Rigidbody2D ridgidBody;
 
     // Start is called before the first frame update
@@ -19,5 +20,7 @@ public class PlayerController : MonoBehaviour
         {
             ridgidBody.AddForce(new Vector2(0,jetpackForce));
         }
+
+        ridgidBody.velocity = new Vector2(forwardMovementSpeed, ridgidBody.velocity.y);
     }
 }
